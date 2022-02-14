fb90b0e3bc0765298f2fa60290a1e8befe725332
Илья Морозкин <morozkin.ilya86@mail.ru>
2022-02-14 08:06:31 +0300

    task24

---
+++ task24/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти кубы чисел от 1 до N
+
+int GetRandomNumber()
+{
+    int numberR = new Random().Next(1, 50);
+    Console.WriteLine($"Число: {numberR}");
+    return numberR;
+}
+int rNumber = GetRandomNumber();
+
+for (int i = 1; i < rNumber; i++)
+
+{
+    int result =(int)Math.Pow((i+1),3);
+    Console.WriteLine($"{i}^3 = {result}");
+}
